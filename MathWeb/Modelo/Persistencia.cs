477477d5a4d1cf1b7a9d05fee4a6f2317fbf2f62
lvejarano <desarrollo20171@outlook.es>
2017-07-05 02:21:06 -0500

    Creacion Clase persistencia

---
+++ MathWeb/Modelo/Persistencia.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Modelo
+{
+    public class Persistencia
+    {
+        string t = "Hola";
+    }
+}
